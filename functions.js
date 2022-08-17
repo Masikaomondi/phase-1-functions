a3084aef305ad822a935f4aba945c90a56e2229f
Masikaomondi <masikaomondi@gmail.com>
2022-08-17 13:38:52 +0300

    functions

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
 function exerciseByronThePoodle() {
-    console.log("Wake Byron the poodle");
-    console.log("Leash Byron the poodle");
-    console.log("Walk to the park Byron the poodle");
-    console.log("Throw the frisbee for Byron the poodle");
-    console.log("Walk home with Byron the poodle");
-    console.log("Unleash Byron the poodle");
+    const dogName = "Byron";
+    const dogBreed = "poodle";
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
   }
